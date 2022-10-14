034af9c51fd572ad5b7c6d809e8a0f1a640f10d2
hudi <huxiaodilw@capinfo.com.cn>
2022-10-14 17:25:08 +0800

    双向链表

---
+++ src/com/atguigu/linkedlist/doublel/DoubleLinkedList.java
@@ -0,0 +1,10 @@
+package com.atguigu.linkedlist.doublel;
+
+/**
+ * @author hudi
+ * @date 2022/10/11 15:59
+ */
+public class DoubleLinkedList {
+
+
+}
---
+++ src/com/atguigu/linkedlist/doublel/DoubleNode.java
@@ -0,0 +1,18 @@
+package com.atguigu.linkedlist.doublel;
+
+/**
+ * @author hudi
+ * @date 2022/10/14 14:27
+ */
+public class DoubleNode {
+    public int val;
+    public String name;
+    public DoubleNode prev;
+    public DoubleNode next;
+
+
+
+
+
+
+}
