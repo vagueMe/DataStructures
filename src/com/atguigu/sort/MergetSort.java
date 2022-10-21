266576e3da73400fccb2f5b14df38a4b7824cbff
hudi <huxiaodilw@capinfo.com.cn>
2022-10-21 18:20:40 +0800

    commit

---
+++ src/com/atguigu/sort/MergetSort.java
@@ -0,0 +1,17 @@
+package com.atguigu.sort;
+
+/**
+ * @author hudi
+ * @date 2022/10/21 17:48
+ */
+public class MergetSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{8,4,5,7,1,3,6,2};
+
+    }
+
+    public static void sort(){
+
+    }
+}
